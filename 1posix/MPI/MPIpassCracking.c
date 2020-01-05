dbdb896009deb67a02558eda07a6511ae757d673
Sonu Sharma <58721146+Sonu123-sp@users.noreply.github.com>
2020-01-06 04:00:27 +0545

    hpc

---
+++ 1posix/MPI/MPIpassCracking.c
@@ -0,0 +1,171 @@
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <crypt.h>
+#include <mpi.h>
+
+/***********************************************************************
+*******
+  Demonstrates how to crack an encrypted password using a simple
+  "brute force" algorithm. Works on passwords that consist only of 2
+uppercase
+  letters and a 2 digit integer. Your personalised data set is included
+in the
+  code.
+
+  Compile with:
+    mpicc -o MPIpassCracking MPIpassCracking.c -lcrypt
+
+  If you want to analyse the results then use the redirection operator
+to send
+  output to a file that you can view using an editor or the less
+utility:
+
+    mpirun -n 3 ./MPIpassCracking
+
+  Dr Kevan Buckley, University of Wolverhampton, 2018
+************************************************************************
+******/
+
+int n_passwords = 4;
+pthread_t t1, t2;
+char *encrypted_passwords[] = {
+
+"$6$KB$yPvAP5BWyF7oqOITVloN9mCAVdel.65miUZrEel72LcJy2KQDuYE6xccHS2ycoxqXDzW.lvbtDU5HuZ733K0X0",
+
+"$6$KB$G/Nlf9jJdkbjMHMqOD0scV20t6cnhtojQKz8v14fhG26GmKOiz.Bd4QXDLeh9SjiEqsBozC812Yf5bvbFWz131",
+
+"$6$KB$ciaKBZs/3LSZlaDE.dagGO0ZE9YfYymWUpzFnIuDTUviDxpqsUVE0HE1/HUXgMp0xl6IfVEzDljEW1iy6ttlG.",
+
+"$6$KB$DY2hmqaRHB3ILUMAMW9k0b.1iHkAfzAe5RtI/stVutttQ.IIV3GcFyQzepzzQhlyGr5nPxIchG6Im8/hYNhzj1"
+};
+
+void substr(char *dest, char *src, int start, int length){
+  memcpy(dest, src + start, length);
+  *(dest + length) = '\0';
+}
+  
+void function1(char *salt_and_encrypted){
+  int x, y, z;     // Loop counters
+  char salt[7];    // String used in hashing the password. Need space
+
+  char plain[7];   // The combination of letters currently being checked
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+  
+  for(x='A'; x<='M'; x++){
+    for(y='A'; y<='Z'; y++){
+      for(z=0; z<=99; z++){
+	printf("Instance number 1:");
+	sprintf(plain, "%c%c%02d",x, y, z);
+	enc = (char *) crypt(plain, salt);
+	count++;
+	if(strcmp(salt_and_encrypted, enc) == 0){
+	  printf("#%-8d%s %s\n", count, plain, enc);
+	} else {
+	  printf(" %-8d%s %s\n", count, plain, enc);
+	}
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+void function2(char *salt_and_encrypted){
+  int x, y, z;     // Loop counters
+  char salt[7];    // String used in hashing the password. Need space
+
+  char plain[7];   // The combination of letters currently being checked
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+  
+  for(x='N'; x<='Z'; x++){
+    for(y='A'; y<='Z'; y++){
+      for(z=0; z<=99; z++){
+	printf("Instance number 2:");
+	sprintf(plain, "%c%c%02d",x, y, z);
+	enc = (char *) crypt(plain, salt);
+	count++;
+	if(strcmp(salt_and_encrypted, enc) == 0){
+	  printf("#%-8d%s %s\n", count, plain, enc);
+	} else {
+	  printf(" %-8d%s %s\n", count, plain, enc);
+	}
+      }
+    }
+  }
+  printf("%d solutions explored\n", count);
+}
+
+
+int time_difference(struct timespec *start, struct timespec *finish,
+                    long long int *difference) {
+  long long int ds =  finish->tv_sec - start->tv_sec; 
+  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+
+  if(dn < 0 ) {
+    ds--;
+    dn += 1000000000; 
+  } 
+  *difference = ds * 1000000000 + dn;
+  return !(*difference > 0);
+}
+
+int main(int argc, char** argv) {
+ struct timespec start, finish;   
+  long long int time_elapsed;
+
+  clock_gettime(CLOCK_MONOTONIC, &start);
+
+ 
+  int size, rank;
+int i;
+
+  MPI_Init(NULL, NULL);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(size != 3) {
+    if(rank == 0) {
+      printf("This program needs to run on exactly 3 processes\n");
+    }
+  } else {
+    if(rank ==0){
+
+      int x;
+    
+      MPI_Send(&x, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+      MPI_Send(&x, 1, MPI_INT, 2, 0, MPI_COMM_WORLD);
+
+
+ 
+    } else if(rank==1){
+      int number;
+      MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, 
+                         MPI_STATUS_IGNORE);
+        for(i=0;i<n_passwords;i<i++) {
+          function1(encrypted_passwords[i]);
+        }
+      }
+      else{
+      int number;
+      MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, 
+                         MPI_STATUS_IGNORE);
+        for(i=0;i<n_passwords;i<i++) {
+          function2(encrypted_passwords[i]);
+        }
+      }
+    }
+    MPI_Finalize(); 
+ clock_gettime(CLOCK_MONOTONIC, &finish);
+  time_difference(&start, &finish, &time_elapsed);
+  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed,
+         (time_elapsed/1.0e9)); 
+
+  return 0;
+}
